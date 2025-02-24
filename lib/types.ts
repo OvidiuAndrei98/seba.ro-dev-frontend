e279cdb28735748d0b557abf34c63bf8620cfb9c
A N D R E I <72840832+OvidiuAndrei98@users.noreply.github.com>
2025-02-24 12:59:55 +0200

    Add files via upload

--- lib/types.ts
+++ lib/types.ts
@@ -8,20 +8,3 @@ export interface Product {
   stock: number;
   productId: string;
 }
-
-/**
- * Properties for the login component.
- */
-export interface LoginPageProps {
-  params: {
-    /**
-     * Invoked when the sign in button is pressed. Must start the authentication
-     * flow.
-     */
-    onLogin(email: string, password: string): void;
-    /**
-     * When set to `true`, a loading indicator is displayed over the login form.
-     */
-    loggingIn?: boolean;
-  };
-}
